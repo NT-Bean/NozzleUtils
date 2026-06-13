fdc9432d689fadea04d34ee6e038dd8d4c5f3631
Apomger <apollo1837builder@gmail.com>
2026-06-13 13:32:00 -0700

    few8uj9rf8u9ewi9olp

--- src/input_file_check.cpp
+++ src/input_file_check.cpp
@@ -1,34 +1,131 @@
-#include "input_file_check.h"
+#include <iostream>
+#include <fstream>
 
-int inputFileCheck() {
-
-    int value = 0;
-
-    std::vector<const char*> inputFilepaths =
-    {
-        "gamma.txt",
-        "chamber pressure {pa}.txt",
-        "chamber temp.txt",
-        "thrust wanted {n}.txt",
-        "Molecular weight.txt",
-        "ambient pressure {pa}.txt",
-        "O-F ratio.txt",
-        "characteristic chamber length {m}.txt"
-    };
-
-    for(int i = 0; i < (int)inputFilepaths.size(); i++)
-    {
-        std::ifstream gfile(inputFilepaths[i]);
-        if (!gfile.is_open()) {
-            std::cout << "File doesn't exist. Creating it.\n";
-            std::ofstream createFile(inputFilepaths[i]);
-            value += 10;
-            if (!createFile.is_open()) {
-                std::cout << "Failed to create the file.\n";
-                value++;
-            }
+int filecheck() {
+ int value = 0;
+    // gamma in
+    std::ifstream gfile("gamma.txt");
+    if (!gfile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("gamma.txt");
+        value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+              value = value + 1;
         }
+        
     }
 
+ // chamber pressure in
+    std::ifstream cpfile("chamber pressure {pa}.txt");
+    if (!cpfile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("chamber pressure {pa}.txt");
+            value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+            value = value + 1;
+        }
+      
+    }
+
+// cumbustion chamber temp in
+    std::ifstream ctfile("chamber temp.txt");
+    if (!ctfile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("chamber temp.txt");
+            value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+            value = value + 1;
+        }
+  
+    }
+
+//thrust wanted in
+    std::ifstream nfile("thrust wanted {n}.txt");
+    if (!nfile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("thrust wanted {n}.txt");
+     value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+            value = value + 1;
+        }
+        
+    }
+
+//Molecular weight in
+    std::ifstream mwfile("Molecular weight.txt");
+    if (!mwfile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("Molecular weight.txt");
+         value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+            value = value + 1;
+        }
+        
+    }
+
+//ambient pressure
+    std::ifstream apafile("ambient pressure {pa}.txt");
+    if (!apafile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("ambient pressure {pa}.txt");
+         value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+            value = value + 1;
+        }
+        
+    }
+
+
+//oxidizer to fuel ratio
+    std::ifstream offile("O-F ratio.txt");
+    if (!offile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("O-F ratio.txt");
+         value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+            value = value + 1;
+        }
+        
+    }
+    //characteristic chamber length
+    std::ifstream cclfile("characteristic chamber length {m}.txt");
+    if (!cclfile.is_open()) {
+        std::cout << "File doesn't exist. Creating it.\n";
+        std::ofstream createFile("characteristic chamber length {m}.txt");
+         value = value + 10;
+        if (!createFile.is_open()) {
+            std::cout << "Failed to create the file.\n";
+            value = value + 1;
+        }
+        
+    }
     return value;
+}
+
+
+int main() {
+    /*
+     this part just checks if thy files exist
+      if you get a number in the one digit you effed up
+    if you get a number in the tens digit just rerun it.
+     */
+int checkfile = filecheck();
+if (checkfile >= 1){
+    std::cout << (checkfile);
+    std::exit(0);
+}
+if (checkfile == 0){
+    std::cout << "Inputs Exist\n";
+}
+    return 0;
+
+//now this is the fun math:
+
 }
