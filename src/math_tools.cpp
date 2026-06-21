374daaef1e995e76584b644d28c338a1d18facd9
NT-Bean <samrahmani2010@gmail.com>
2026-06-21 12:04:35 -0700

    doodoobug

--- src/math_tools.cpp
+++ src/math_tools.cpp
@@ -1,6 +1,34 @@
-#include "math_tools.h"
+// put math formulae here
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "Math_tools.h"
 
-void MathTools::setStdOutPrecision(int decimalPlaces)
-{
-    std::cout << std::fixed << std::setprecision(decimalPlaces);
-}
+// formula 1
+int formula1(int a, int b){
+
+};
+
+
+// formula 2
+int formula2(int a,int b){
+
+};
+
+
+// formula 3
+int formula3(int a, int b){
+
+};
+
+
+// formula 4
+int formula4(int a, int b){
+
+};
+
+
+// formula 5
+int formula5(int a, int b){
+
+};
